a868821cb7438f5972ebe33942d9f7b65d1982be
MarinoLinic <84214511+MarinoLinic@users.noreply.github.com>
2022-10-08 18:40:26 +0200

    Added React Markdown and finished script

--- public/getposts.cjs
+++ public/getposts.cjs
@@ -4,8 +4,8 @@ const fs = require('fs')
 const dirPath = path.join(__dirname, '../src/assets/content/rg')
 let postList = []
 
-const getPosts = async () => {
-	await fs.readdir(dirPath, (err, files) => {
+const getPosts = () => {
+	fs.readdir(dirPath, (err, files) => {
 		if (err) return console.log('Failed to list contents of directory: ' + err)
 		files.forEach((file, index) => {
 			let obj = {}
@@ -23,16 +23,35 @@ const getPosts = async () => {
 						metadata.forEach((line) => {
 							obj[line.split(': ')[0]] = line.split(': ')[1]
 						})
-						console.log(obj)
 						return obj
 					}
+				}
+				const parseContent = ({ lines, metadataIndices }) => {
+					if (metadataIndices.length > 0) {
+						lines = lines.slice(metadataIndices[1] + 1, lines.length)
+					}
+					return lines.join('\n')
 				}
 				const lines = contents.split('\n')
 				const metadataIndices = lines.reduce(getMetadataIndices, [])
 				const metadata = parseMetadata({ lines, metadataIndices })
+				const content = parseContent({ lines, metadataIndices })
+				post = {
+					id: index + 1,
+					title: metadata.title ? metadata.title : 'No title available',
+					author: metadata.author ? metadata.author : 'No author available',
+					date: metadata.date ? metadata.date : 'No date available',
+					content: metadata.content ? metadata.content : 'No content available'
+				}
+				postList.push(post)
+				if (index === files.length - 1) {
+					let data = JSON.stringify(postList)
+					fs.writeFileSync('src/assets/rgPosts.json', data)
+				}
 			})
 		})
 	})
+	return
 }
 
 getPosts()
