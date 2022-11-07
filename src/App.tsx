f7db1ed02adc30e19d58b5bb36fbff45ed8a593f
MarinoLinic <84214511+MarinoLinic@users.noreply.github.com>
2022-11-07 03:07:28 +0100

    Some timers

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,22 @@ function App() {
 					path="/countdown"
 					element={<Countdown date="October 13, 2023, 00:00" title="Time until Marino's 24th birthday" />}
 				/>
+				<Route
+					path="/math"
+					element={<Countdown date="November 28, 2022, 16:00" title="Time until first Math exam" />}
+				/>
+				<Route
+					path="/1"
+					element={<Countdown date="November 15, 2022, 00:00" title="Time until the second RG task" />}
+				/>
+				<Route
+					path="/2"
+					element={<Countdown date="November 28, 2022, 00:00" title="Time until the first MT task" />}
+				/>
+				<Route
+					path="/3"
+					element={<Countdown date="December 8, 2022, 12:00" title="Time until the Django exam" />}
+				/>
 				<Route path="/rg" element={<RacunalnaGrafika />} />
 				<Route path="/rg/:id" element={<Post />} />
 
