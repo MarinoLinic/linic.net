b050ad9daad0882ae3bf4862acb397a2cbb01cae
MarinoLinic <84214511+MarinoLinic@users.noreply.github.com>
2022-11-08 01:54:21 +0100

    Added blank screen if not fully loaded

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,61 +10,63 @@ const Home = () => {
 	})
 
 	const [isHovering, setIsHovering] = useState(false)
+	const [loading, setLoading] = useState(true)
 
 	useEffect(() => {
 		localStorage.setItem('Clicks', JSON.stringify(count))
+		setLoading(false)
 	}, [count])
 
-	return (
-		<>
-			<div className="">
-				<Navigation />
-				<div className="flex flex-col items-center justify-center h-[60vh] md:h-[80vh]">
-					{isHovering ? (
-						<h5
-							onMouseOut={() => {
-								handleMouseOut(setIsHovering)
-							}}
-							className="text-secondary uppercase cursor-default">
-							& musician, photographer
-						</h5>
-					) : (
-						<h5
-							onMouseOver={() => {
-								handleMouseOver(setIsHovering)
-							}}
-							className="text-quarternary uppercase cursor-default">
-							junior web developer
-						</h5>
-					)}
+	if (!loading) {
+		return (
+			<>
+				<div className="">
+					<Navigation />
+					<div className="flex flex-col items-center justify-center h-[60vh] md:h-[80vh]">
+						{isHovering ? (
+							<h5
+								onMouseOut={() => {
+									handleMouseOut(setIsHovering)
+								}}
+								className="text-secondary uppercase cursor-default">
+								& musician, photographer
+							</h5>
+						) : (
+							<h5
+								onMouseOver={() => {
+									handleMouseOver(setIsHovering)
+								}}
+								className="text-quarternary uppercase cursor-default">
+								junior web developer
+							</h5>
+						)}
 
-					<h1 className="cursor-default">Marino Linić</h1>
-					<div className="my-4 flex justify-center">
-						<img
-							src="ml.jpg"
-							alt="Image of Marino Linic"
-							className="w-36 h-36 grayscale hover:grayscale-0 rounded-3xl"
-						/>
+						<h1 className="cursor-default">Marino Linić</h1>
+						<div className="my-4 flex justify-center">
+							<img
+								src="ml.jpg"
+								alt="Image of Marino Linic"
+								className="w-36 h-36 grayscale hover:grayscale-0 rounded-3xl"
+							/>
+						</div>
+						<h2 className="">Hello!</h2>
+						<p className="">
+							Visit my <a href="https://linktr.ee/marino.linic">social media</a>!
+						</p>
+						<div className="my-4">
+							<button onClick={() => setCount((count: number) => count + 1)}>count is {count}</button>
+						</div>
+						<Circles />
 					</div>
-					<h2 className="">Hello!</h2>
-					<p className="">
-						Visit my <a href="https://linktr.ee/marino.linic">social media</a>!
-					</p>
-					<div className="my-4">
-						<button onClick={() => setCount((count: number) => count + 1)}>count is {count}</button>
-					</div>
-					<Circles />
 				</div>
-			</div>
-		</>
-	)
+			</>
+		)
+	}
 }
 
 export default Home
 
 /*
-const [loading, setLoading] = useState(true)
-
 {loading && (
 						<div className="my-4 w-36 h-36 pt-3 animate-pulse">
 							<ML />
