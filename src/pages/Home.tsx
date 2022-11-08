16ab6a1037a04b1951a476471d17394bb2d9e37a
MarinoLinic <84214511+MarinoLinic@users.noreply.github.com>
2022-11-08 01:42:56 +0100

    Some loader ideas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
 import Circles from '../components/Circles'
-import Loader from '../components/Loader'
 import Navigation from '../components/Navigation'
 import { handleMouseOver, handleMouseOut } from '../utils/functions/handleMouseOver'
 
@@ -11,64 +10,73 @@ const Home = () => {
 	})
 
 	const [isHovering, setIsHovering] = useState(false)
-	const [loading, setLoading] = useState(false)
 
 	useEffect(() => {
 		localStorage.setItem('Clicks', JSON.stringify(count))
-
-		// setLoading(true)
-		// setTimeout(() => {
-		// 	setLoading(false)
-		// }, 3000)
 	}, [count])
 
 	return (
 		<>
-			{loading ? (
-				<Loader />
-			) : (
-				<div className="">
-					<Navigation />
-					<div className="flex flex-col items-center justify-center h-[60vh] md:h-[80vh]">
-						{isHovering ? (
-							<h5
-								onMouseOut={() => {
-									handleMouseOut(setIsHovering)
-								}}
-								className="text-secondary uppercase cursor-default">
-								& musician, photographer
-							</h5>
-						) : (
-							<h5
-								onMouseOver={() => {
-									handleMouseOver(setIsHovering)
-								}}
-								className="text-quarternary uppercase cursor-default">
-								junior web developer
-							</h5>
-						)}
+			<div className="">
+				<Navigation />
+				<div className="flex flex-col items-center justify-center h-[60vh] md:h-[80vh]">
+					{isHovering ? (
+						<h5
+							onMouseOut={() => {
+								handleMouseOut(setIsHovering)
+							}}
+							className="text-secondary uppercase cursor-default">
+							& musician, photographer
+						</h5>
+					) : (
+						<h5
+							onMouseOver={() => {
+								handleMouseOver(setIsHovering)
+							}}
+							className="text-quarternary uppercase cursor-default">
+							junior web developer
+						</h5>
+					)}
 
-						<h1 className="cursor-default">Marino Linić</h1>
-						<div className="my-4 flex justify-center">
-							<img
-								src="ml.jpg"
-								alt="Image of Marino Linic"
-								className="w-36 h-36 grayscale hover:grayscale-0 rounded-3xl"
-							/>
-						</div>
-						<h2 className="">Hello!</h2>
-						<p className="">
-							Visit my <a href="https://linktr.ee/marino.linic">social media</a>!
-						</p>
-						<div className="my-4">
-							<button onClick={() => setCount((count: number) => count + 1)}>count is {count}</button>
-						</div>
-						<Circles />
+					<h1 className="cursor-default">Marino Linić</h1>
+					<div className="my-4 flex justify-center">
+						<img
+							src="ml.jpg"
+							alt="Image of Marino Linic"
+							className="w-36 h-36 grayscale hover:grayscale-0 rounded-3xl"
+						/>
 					</div>
+					<h2 className="">Hello!</h2>
+					<p className="">
+						Visit my <a href="https://linktr.ee/marino.linic">social media</a>!
+					</p>
+					<div className="my-4">
+						<button onClick={() => setCount((count: number) => count + 1)}>count is {count}</button>
+					</div>
+					<Circles />
 				</div>
-			)}
+			</div>
 		</>
 	)
 }
 
 export default Home
+
+/*
+const [loading, setLoading] = useState(true)
+
+{loading && (
+						<div className="my-4 w-36 h-36 pt-3 animate-pulse">
+							<ML />
+						</div>
+						)
+}
+<div className={`my-4 flex justify-center ${loading ? 'hidden' : ''}`}>
+	<img
+		src="ml.jpg"
+		alt="Image of Marino Linic"
+		onLoad={() => setLoading(false)}
+		className="w-36 h-36 grayscale hover:grayscale-0 rounded-3xl"
+	/>
+</div>
+*/
