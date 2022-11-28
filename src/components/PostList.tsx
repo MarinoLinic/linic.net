cbe9a1a06b50a4665a9b8480e62b27a03fb185a0
MarinoLinic <84214511+MarinoLinic@users.noreply.github.com>
2022-11-28 15:31:59 +0100

    ..

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -14,9 +14,7 @@ const PostList = () => {
 				<Link to="/" className="text-quarternary hover:text-quarternary">
 					Marina Linića
 				</Link>{' '}
-				(ak. g. 2022/23) za dokumentaciju projekata iz kolegija Računalna grafika.{' '}
-				<a href="https://drive.google.com/drive/folders/1-EbQyVuDqMYiLW8L-u4AKm74FNmhQTF7?usp=share_link">Ovdje</a>{' '}
-				se nalazi direktorij na Google Disku.
+				(ak. g. 2022/23) za dokumentaciju projekata iz kolegija Računalna grafika.
 			</h5>
 			<div className="grid grid-cols-1 md:grid-cols-3 my-16 gap-8 md:gap-48">
 				{postList.length &&
