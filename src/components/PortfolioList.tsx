1ae8ce6981facbdd2910c651fe28b8435f530dee
MarinoLinic <marinolinic@gmail.com>
2024-01-14 04:40:02 +0100

    -4

--- src/components/PortfolioList.tsx
+++ src/components/PortfolioList.tsx
@@ -1,15 +1,22 @@
+import Linic from '../assets/content/portfolio/linic.mdx'
 import History from '../assets/content/portfolio/history.mdx'
 import Salary from '../assets/content/portfolio/salary.mdx'
 import Clicker from '../assets/content/portfolio/clicker.mdx'
 import Photography from '../assets/content/portfolio/photography.mdx'
 import Flappy from '../assets/content/portfolio/flappy.mdx'
 import Fidit from '../assets/content/portfolio/fidit.mdx'
+import NavalWars from '../assets/content/portfolio/navalwars.mdx'
+import Insect from '../assets/content/portfolio/insect.mdx'
 import Other from '../assets/content/portfolio/other.mdx'
 
 const PortfolioList = () => {
 	return (
 		<section className="flex items-center justify-center">
 			<div className="md:w-[600px] w-[300px] text-center my-16 items-center justify-center">
+				<Linic />
+				<br /> <br />
+				<hr />
+				<br /> <br />
 				<History />
 				<br /> <br />
 				<hr />
@@ -19,6 +26,14 @@ const PortfolioList = () => {
 				<hr />
 				<br /> <br />
 				<Fidit />
+				<br /> <br />
+				<hr />
+				<br /> <br />
+				<Insect />
+				<br /> <br />
+				<hr />
+				<br /> <br />
+				<NavalWars />
 				<br /> <br />
 				<hr />
 				<br /> <br />
