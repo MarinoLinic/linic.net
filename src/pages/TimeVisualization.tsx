276d775f86ed4ba43162e491104a8c6379f06fb6
MarinoLinic <marinolinic@gmail.com>
2024-02-03 00:25:47 +0100

    Improvements

--- src/pages/TimeVisualization.tsx
+++ src/pages/TimeVisualization.tsx
@@ -5,10 +5,8 @@ import Square from '../components/TimeVisualization_Square'
 import { timeConversion } from '../utils/functions/timeConversion'
 
 interface Params {
-	years: string
-	months: string
-	weeks: string
 	startDate: string
+	endDate: string
 }
 
 const TimeVisualization = () => {
@@ -17,30 +15,32 @@ const TimeVisualization = () => {
 	const [passedWeeks, setPassedWeeks] = useState(0)
 	const [totalWeeks, setTotalWeeks] = useState(0)
 
+	// ------------------- Date calculation
+
+	const today = new Date()
+	const inputStartDate = new Date(startDate) // YYYY-MM-DD
+	const inputEndDate = new Date(endDate) // YYYY-MM-DD
+
+	// Calculating miliseconds
+	const totalDiff = inputEndDate.getTime() - inputStartDate.getTime()
+	const passedDiff = today.getTime() - inputStartDate.getTime()
+
+	// Creating time objects
+	const timeTotal = timeConversion(totalDiff)
+	const timePassed = timeConversion(passedDiff)
+	const timeLeft = timeConversion(totalDiff - passedDiff)
+
+	// Testing via console.log
+	console.log('Percentage passed:', (passedDiff / totalDiff) * 100)
+
+	// ------------------- Updating state
+
 	useEffect(() => {
-		const today = new Date()
-		const inputStartDate = new Date(startDate) // Assuming startDate is in the format 'YYYY-MM-DD'
-		const inputEndDate = new Date(endDate) // Assuming endDate is in the format 'YYYY-MM-DD'
-
-		// Calculating miliseconds
-		const totalDiff = inputEndDate.getTime() - inputStartDate.getTime()
-		const passedDiff = today.getTime() - inputStartDate.getTime()
-
-		// Creating time objects
-		const timeTotal = timeConversion(totalDiff)
-		const timePassed = timeConversion(passedDiff)
-		const timeLeft = timeConversion(totalDiff - passedDiff)
-
-		// Testing via console.log
-		console.log('Percentage passed:', (passedDiff / totalDiff) * 100) // testing
-		console.log(timeLeft)
-		console.log(timePassed)
-		console.log(timeTotal)
-
-		// State
 		setPassedWeeks(timePassed.weeks)
 		setTotalWeeks(timeTotal.weeks)
 	}, [startDate, endDate])
+
+	// ------------------- Rendering the visualization
 
 	const renderSquares = () => {
 		const squares = []
@@ -54,7 +54,41 @@ const TimeVisualization = () => {
 		return squares
 	}
 
-	return <div style={{ display: 'flex', flexWrap: 'wrap' }}>{renderSquares()}</div>
+	// ------------------- JSX
+
+	return (
+		<div className="mx-48 my-12">
+			<div className="my-8">
+				<h2 className="text-center my-8">
+					{startDate} ... to ... {endDate}
+				</h2>
+				<div className="flex flex-row justify-between">
+					<section>
+						{Object.entries(timeTotal).map(([key, value]) => (
+							<p key={key}>
+								{'Total ' + key.charAt(0).toUpperCase() + key.slice(1)}: {value}
+							</p>
+						))}
+					</section>
+					<section>
+						{Object.entries(timePassed).map(([key, value]) => (
+							<p key={key}>
+								{'Passed ' + key.charAt(0).toUpperCase() + key.slice(1)}: {value}
+							</p>
+						))}
+					</section>
+					<section>
+						{Object.entries(timeLeft).map(([key, value]) => (
+							<p key={key}>
+								{'Left ' + key.charAt(0).toUpperCase() + key.slice(1)}: {value}
+							</p>
+						))}
+					</section>
+				</div>
+			</div>
+			<div style={{ display: 'flex', flexWrap: 'wrap' }}>{renderSquares()}</div>
+		</div>
+	)
 }
 
 export default TimeVisualization
--- src/utils/functions/timeConversion.ts
+++ src/utils/functions/timeConversion.ts
@@ -1,12 +1,12 @@
 export function timeConversion(time: number) {
 	const miliseconds = time
-	const seconds = Math.floor(time / 1000)
-	const minutes = Math.floor(time / (1000 * 60))
-	const hours = Math.floor(time / (1000 * 60 * 60))
-	const days = Math.floor(time / (1000 * 60 * 60 * 24))
-	const weeks = Math.floor(time / (1000 * 60 * 60 * 24 * 7))
-	const months = Math.floor(time / (1000 * 60 * 60 * 24 * 30.437))
-	const years = Math.floor(time / (1000 * 60 * 60 * 24 * 365.25))
+	const seconds = Math.round(time / 1000)
+	const minutes = Math.round(time / (1000 * 60))
+	const hours = Math.round(time / (1000 * 60 * 60))
+	const days = Math.round(time / (1000 * 60 * 60 * 24))
+	const weeks = Math.round(time / (1000 * 60 * 60 * 24 * 7))
+	const months = Math.round(time / (1000 * 60 * 60 * 24 * 30.437))
+	const years = Math.round(time / (1000 * 60 * 60 * 24 * 365.25))
 	const decades = Math.round(time / (1000 * 60 * 60 * 24 * 365.25 * 10))
 
 	return {
